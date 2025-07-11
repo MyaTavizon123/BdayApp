7da8fb264fee0e7615fd0dd832406823be97dd27
Mya Tavizon <mya.td12@gmail.com>
2025-07-11 09:30:29 -0500

    Initial Commit

---
+++ BdayApp/BdayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BdayAppApp.swift
+//  BdayApp
+//
+//  Created by Scholar on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BdayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
